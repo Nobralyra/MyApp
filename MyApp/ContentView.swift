1c5297ddacf74538c60ccf162f60ee3ce4d345e6
Nobralyra <signees96@gmail.com>
2020-09-14 09:33:57 +0200

    Initial Commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by admin on 14/09/2020.
+//  Copyright © 2020 Signe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
